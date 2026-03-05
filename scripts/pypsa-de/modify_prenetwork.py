6aaa7ce7d64dd85a5eee5de9d775d2d008f67bd8
cpschau <caspar.schauss@ieg.fraunhofer.de>
2026-03-05 17:35:40 +0100

    fix(modify_prenetwork): replace deprecated import_components_from_dataframe
    Replace all n.import_components_from_dataframe(df, 'Link') calls with
    n.add('Link', df.index, **df) to use the current PyPSA API.

--- scripts/pypsa-de/modify_prenetwork.py
+++ scripts/pypsa-de/modify_prenetwork.py
@@ -939,7 +939,7 @@ def add_hydrogen_turbines(n):
         h2_plants.bus2 = ""
         h2_plants.efficiency2 = 1
         # add the new links
-        n.import_components_from_dataframe(h2_plants, "Link")
+        n.add("Link", h2_plants.index, **h2_plants)
 
     # special handling of CHPs
     gas_plants = n.links[
@@ -953,7 +953,7 @@ def add_hydrogen_turbines(n):
     h2_plants.bus0 = h2_plants.bus1 + " H2"
     h2_plants.bus3 = ""
     h2_plants.efficiency3 = 1
-    n.import_components_from_dataframe(h2_plants, "Link")
+    n.add("Link", h2_plants.index, **h2_plants)
 
 
 def force_retrofit(n, params):
@@ -996,7 +996,7 @@ def force_retrofit(n, params):
         h2_plants.capital_cost *= 1 + params["cost_factor"]
         h2_plants.overnight_cost *= 1 + params["cost_factor"]
         # add the new links
-        n.import_components_from_dataframe(h2_plants, "Link")
+        n.add("Link", h2_plants.index, **h2_plants)
         n.links.drop(gas_plants, inplace=True)
 
     # special handling of CHPs
@@ -1017,7 +1017,7 @@ def force_retrofit(n, params):
     h2_plants.efficiency3 = 1  # default value
     h2_plants.capital_cost *= 1 + params["cost_factor"]
     h2_plants.overnight_cost *= 1 + params["cost_factor"]
-    n.import_components_from_dataframe(h2_plants, "Link")
+    n.add("Link", h2_plants.index, **h2_plants)
     n.links.drop(gas_plants, inplace=True)
 
 
